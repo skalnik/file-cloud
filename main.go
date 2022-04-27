d5b3055de95d73c6a5b4f0a5503e921c13e15598
Mike Skalnik <mike@skalnik.com>
2022-04-26 20:20:43 -0700

    ⛳️

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"mime/multipart"
 	"net/http"
@@ -14,17 +15,37 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
+type Config struct {
+	bucket string
+	key    string
+	secret string
+	port   string // https://twitter.com/keith_duncan/status/638582305917833217
+}
+
+var s3Client *s3.Client
+var fileCloudConfig Config
+
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
+	flag.StringVar(&fileCloudConfig.bucket, "bucket", lookupEnvDefault("BUCKET", "file-cloud"), "AWS S3 Bucket name to store files in")
+	flag.StringVar(&fileCloudConfig.key, "key", lookupEnvDefault("KEY", "ABC123"), "AWS Key to use")
+	flag.StringVar(&fileCloudConfig.secret, "secret", lookupEnvDefault("SECRET", "ABC/123"), "AWS Secret to use")
+	flag.StringVar(&fileCloudConfig.port, "port", lookupEnvDefault("PORT", "8080"), "Port to listen on") // https://twitter.com/keith_duncan/status/638582305917833217
+	flag.Parse()
+
+	creds := credentials.NewStaticCredentialsProvider(fileCloudConfig.key, fileCloudConfig.secret, "")
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithCredentialsProvider(creds), config.WithRegion("us-west-1"))
+	if err != nil {
+		fmt.Println("Shits fucked")
+		os.Exit(1)
 	}
+
+	s3Client = s3.NewFromConfig(cfg)
 
 	fs := http.FileServer(http.Dir("static/"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.HandleFunc("/", index)
 
-	http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
+	http.ListenAndServe(fmt.Sprintf(":%s", fileCloudConfig.port), nil)
 }
 
 func index(writer http.ResponseWriter, request *http.Request) {
@@ -58,22 +79,25 @@ func index(writer http.ResponseWriter, request *http.Request) {
 }
 
 func upload(file multipart.File) (bool, error) {
-	// https://481495170185.signin.aws.amazon.com/console
-	key := os.Getenv("KEY")
-	secret := os.Getenv("SECRET")
-	creds := credentials.NewStaticCredentialsProvider(key, secret, "")
-
-	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithCredentialsProvider(creds), config.WithRegion("us-west-1"))
-	if err != nil {
-		return false, err
-	}
-
-	awsS3Client := s3.NewFromConfig(cfg)
-	_, err = awsS3Client.PutObject(context.TODO(), &s3.PutObjectInput{
-		Bucket: aws.String("file-cloud-test"),
+	_, err := s3Client.PutObject(context.TODO(), &s3.PutObjectInput{
+		Bucket: aws.String(fileCloudConfig.bucket),
 		Key:    aws.String("lmaolol"),
 		Body:   file,
 	})
 
+	if err != nil {
+		return false, err
+	}
+
 	return true, nil
+}
+
+func lookupEnvDefault(envKey, defaultValue string) string {
+	value, exists := os.LookupEnv(envKey)
+
+	if exists {
+		return value
+	} else {
+		return defaultValue
+	}
 }
